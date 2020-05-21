51cd12f04e4b622d5972c35665be6a5b969f0267
Zana H <zhertica@gmail.com>
2020-05-21 03:08:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key]
     hash[key] += 1
-else
-  hash[key] = 1
-end
+  else
+    hash[key] = 1   
+  end
 end
